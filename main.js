c6e957d26a4b83299c9fab8599296d463d0ba70e
水靈 <z23320785@gmail.com>
2024-05-05 00:37:21 +0800

    add electron init

---
+++ main.js
@@ -0,0 +1,57 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow,globalShortcut } = require('electron')
+//var Mousetrap = require('Mousetrap');
+const path = require('node:path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.on('before-input-event', (event, input) => {
+    if (input.control && input.key.toLowerCase() === 'i') {
+      console.log('Pressed Control+I')
+      event.preventDefault()
+    }
+  })
+
+  //Mousetrap.bind('4', () => { console.log('4') })
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  globalShortcut.register('CommandOrControl+T', () => {
+    console.log('Electron loves global shortcuts!')
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ renderer.js
@@ -0,0 +1,8 @@
+function handleKeyPress (event) {
+    // You can put code here to handle the keypress.
+    document.getElementById('last-keypress').innerText = event.key
+    //alert(`You pressed ${event.key}`);
+    console.log(`You pressed ${event.key}`)
+  }
+  
+  window.addEventListener('keyup', handleKeyPress, true)
